94d2a97e79f629c00b31c906f79f5a06cae8c840
vjmadne123 <100928706+vjmadne123@users.noreply.github.com>
2022-04-12 17:53:15 +0530

    Day23

--- Applet1.java
+++ Applet1.java
@@ -1,28 +1,52 @@
-package Day24;
+package Day23;
 
 import java.applet.Applet;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseMotionListener;
+import java.awt.Button;
+import java.awt.TextField;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
-public class Applet1 extends Applet{
-
+public class Applet1 extends Applet {
+	Button b1, b2;
+	TextField t;
 	public void init() {
-		//add(b1 = new Button("Vlick"));
-		//b1.addActionListener();
-		addMouseMotionListener(new MouseMotionAdapter () {
+		add(b1 = new Button("Click"));
+		add(t = new TextField(15));
+		add(b2 = new Button("Clear"));
+		b1.addFocusListener(new FocusListener() {
+
 			@Override
-			public void mouseMoved(MouseEvent e) {
-				int x = e.getX();
-				int y = e.getY();
-				String i = String.valueOf(x);
-				String j = String.valueOf(y);
-				showStatus(i+", "+ String.valueOf(y));
+			public void focusGained(FocusEvent arg0) {
+				// TODO Auto-generated method stub
+				t.setText("hello");
+			}
+
+			@Override
+			public void focusLost(FocusEvent arg0) {
+				// TODO Auto-generated method stub
+				t.setText(" ");
+			}
+
+	});
+		b2.addFocusListener(new FocusListener() {
+
+			@Override
+			public void focusGained(FocusEvent e) {
+				// TODO Auto-generated method stub
+				t.setText("welcome");
+			}
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				// TODO Auto-generated method stub
+				t.setText("");
 				
 			}
+			
 		});
-		
-	
 	}
+	
+	
+
 
 }
