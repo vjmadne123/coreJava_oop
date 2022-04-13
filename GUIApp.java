5051f37c069d04b5e76e46c61d823877ff009e7b
vjmadne123 <100928706+vjmadne123@users.noreply.github.com>
2022-04-13 11:33:08 +0530

    Day25

--- GUIApp.java
+++ GUIApp.java
@@ -1,24 +1,38 @@
 package Day24;
 
 import java.awt.Button;
+import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.TextField;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 public class GUIApp extends Frame{
 	Button b1,b2;
 	TextField t;
 	public GUIApp() {
+		setLayout(new FlowLayout());
 		add(b1 = new Button("Click"));
 		add(t = new TextField(15));
 		add(b2 = new Button("Clear"));
 		b1.addActionListener((e) ->t.setText("Hello"));
 		b2.addActionListener((e) ->t.setText(" "));
-		addWindowListener(new WindowListener());
-		
+		addWindowListener(new WindowAdapter(){
+			@Override
+			public void windowClosed(WindowEvent e) {
+				// TODO Auto-generated method stub
+				System.exit(0);
+			}			
+		});
+		setSize(400, 500);
+		setVisible(false);
 		
 }
 	public static  void main (String[] args) {
+		
+		new GUIApp();
+		
 		
 	}
 }
